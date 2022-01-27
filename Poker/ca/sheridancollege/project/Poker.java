fc9d11565f7b2b86134020805ffa18b9a3f87ae8
Nuggethead2003 <amirrezaghiassian7@gmail.com>
2022-01-26 22:28:33 -0500

    I worked on it a bit, idk I fixed smth

--- Poker/ca/sheridancollege/project/Poker.java
+++ Poker/ca/sheridancollege/project/Poker.java
@@ -1,9 +1,12 @@
-package Poker;
+package Poker.ca.sheridancollege.project;
 class Poker{
     private static int globalId = 0;
     public final int GAME_ID = globalId++;
     private int initialChips;
     
+    public void play(int a){
+
+    }
     public Poker(int initialChips){
         this.initialChips = initialChips;
     }
